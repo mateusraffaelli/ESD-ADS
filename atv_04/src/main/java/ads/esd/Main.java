13c428b7ac5d8b2e4c79a40b89c94a3a9cf5e22b
Mateus Raffaelli <mattewscastro@gmail.com>
2026-09-23 16:16:18 -0300

    Corrigindo erros no loop do labirinto

--- atv_04/src/main/java/ads/esd/Main.java
+++ atv_04/src/main/java/ads/esd/Main.java
@@ -6,12 +6,11 @@ public class Main {
     static void main() {
         Labirinto labirinto = new Labirinto();
 
-        while (!labirinto.caminhoPossivel()){
+        while (!labirinto.verificarPosicao()){
             labirinto.imprimir();
             IO.readln("Digite ENTER para continuar");
-
-
         }
-
+        labirinto.imprimir();
+        System.out.println("Final encontrado");
     }
 }
